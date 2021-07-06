b5349aa3491447b96f040e8944cdba633efe10fb
YoungBirds <huynguyen2913@gmail.com>
2021-07-06 23:07:43 +0700

    tạo Project

---
+++ Shop_Quan_Ao/Shop_Quan_Ao/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Shop_Quan_Ao
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Shop_Quan_Ao/Shop_Quan_Ao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shop_Quan_Ao.MvcApplication" Language="C#" %>
